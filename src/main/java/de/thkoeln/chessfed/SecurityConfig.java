6b01798d001f48a4bd079c6e21da7103b5f7eff1
Timo Ley <timo.ley@tilera.dev>
2026-01-17 15:29:50 +0100

    fix: allow POST requests

--- src/main/java/de/thkoeln/chessfed/SecurityConfig.java
+++ src/main/java/de/thkoeln/chessfed/SecurityConfig.java
@@ -10,7 +10,7 @@ public class SecurityConfig {
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 
