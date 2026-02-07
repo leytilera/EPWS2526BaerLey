8dba62fb660db01badf536f0045ef0e5b48d55cd
Timo Ley <timo.ley@tilera.dev>
2026-02-07 16:11:40 +0100

    feat: simple game switcher

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -16,35 +16,35 @@ document.addEventListener("DOMContentLoaded", () => {
 window.addEventListener('hashchange', (event) => {
     event.preventDefault();
     state.gameid = window.location.hash.substring(1);
-    console.log(state.gameid);
+    loadGame();
 });
 
 async function onMessage(msg) {
-    console.log(msg);
     if (msg.type == 3 && msg.context == state.gameid) {
         let moveStr = objToJocly(msg.data);
         let move = await state.match.pickMove(moveStr);
         await state.match.playMove(move);
         requestUserInput(state.send);
+    } else if (msg.type == 0) {
+        addGameToList(msg.context);
     }
 }
 
 async function main() {
     state.user = await getUserData().username;
     state.send = await initWebsocket(onMessage);
-    
+    let games = await getGames();
+    games.forEach(game => {
+        addGameToList(game.id);
+    });
+
     state.match = await Jocly.createMatch("classic-chess");
     
     let element = document.querySelector("[data-js-board]");
     await state.match.attachElement(element);
+    await state.match.setViewOptions({skin: "skin2dfull"});
 
-    let gameState = await getGameState(state.gameid);
-    let init = gameToJoclyState(gameState);
-    await state.match.load(init);
-
-    if (gameState.yourTurn) {
-        await requestUserInput(state.send);
-    }
+    await loadGame();
     
 }
 
@@ -66,4 +66,26 @@ async function getGameState(gameid) {
 
 async function getUserData() {
     return await fetch(`/api/user`).then((res) => res.json());
+}
+
+async function getGames() {
+    return await fetch(`/api/games`).then((res) => res.json());
+}
+
+async function loadGame() {
+    if (!state.gameid) return;
+
+    let gameState = await getGameState(state.gameid);
+    let init = gameToJoclyState(gameState);
+    await state.match.load(init);
+
+    if (gameState.yourTurn) {
+        await requestUserInput(state.send);
+    }
+}
+
+async function addGameToList(gameId) {
+    let element = document.querySelector("[data-js-gamelist]");
+    let entry = `<li><a href="#${gameId}">${gameId}</a></li>`;
+    element.innerHTML += entry;
 }
